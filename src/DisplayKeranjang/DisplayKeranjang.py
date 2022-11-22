c6c7d0c883f1c6c4cd08e0b465b17163822022c6
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2022-11-22 15:15:54 +0700

    Update DisplayKeranjang.py

--- src/DisplayKeranjang/DisplayKeranjang.py
+++ src/DisplayKeranjang/DisplayKeranjang.py
@@ -1,29 +1,3 @@
-
-import psycopg2
-
-class DisplayKeranjang:
-    def displayKeranjang():
-        def getKeranjang():
-            try:
-                return psycopg2.connect(
-                    database="DataRestoran",
-                    user="postgres",
-                    password="postgres",
-                    host="127.0.0.1",
-                    port=5432,
-                )
-            except:
-                return False
-
-        conn = getKeranjang()
-        curr = conn.cursor()
-        curr.execute("SELECT * FROM datapesanancustomer;")
-        data = curr.fetchall()
-        for row in data:
-            print(row)
-        conn.close()
-#test
-
 import psycopg2
 
 class DisplayKeranjang:
