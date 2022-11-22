a7cd9932e10bf614d519966b3f348b20a1d19fe7
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2022-11-22 18:10:17 +0700

    DeleteMenu.py

---
+++ src/DisplayKeranjang/DeleteMenu.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+def deleteMenu(id_barang):
+    try:
+        connection = psycopg2.connect(
+                    database="DataRestoran",
+                    user="postgres",
+                    password="123",
+                    host="127.0.0.1",
+                    port=5432)
+
+        cursor = connection.cursor()
+
+        # Update single record now
+        sql_delete_query = """Delete from datapesanancustomer where id_barang = %s"""
+        cursor.execute(sql_delete_query, (id_barang,))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Record deleted successfully ")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error in Delete operation", error)
+
+deleteMenu(1)
