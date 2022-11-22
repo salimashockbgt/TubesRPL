6cae09b521c3e164c435d9615a3e323251d109ed
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2022-11-22 14:03:37 +0700

    Add files via upload

---
+++ src/DisplayKeranjang/DisplayKeranjang.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+class DisplayKeranjang:
+    def displayKeranjang():
+        def getKeranjang():
+            try:
+                return psycopg2.connect(
+                    database="DataRestoran",
+                    user="postgres",
+                    password="123",
+                    host="127.0.0.1",
+                    port=5432,
+                )
+            except:
+                return False
+
+        conn = getKeranjang()
+        curr = conn.cursor()
+        curr.execute("SELECT * FROM datapesanancustomer;")
+        data = curr.fetchall()
+        for row in data:
+            print(row)
+        conn.close()
+#test
+    displayKeranjang()
